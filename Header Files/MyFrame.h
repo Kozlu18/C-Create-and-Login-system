eff6ed1ee82bb57671356dbd9a39ab343b00e59b
Yusuf <tekinyusufselim@gmail.com>
2026-02-15 17:39:14 +0300

    Add new member variables to MyFrame class

--- Header Files/MyFrame.h
+++ Header Files/MyFrame.h
@@ -11,16 +11,25 @@ private :
 	void Login(wxCommandEvent& evt);
 	void CheckAccount(wxCommandEvent& evt);
 	void OnReturn(wxCommandEvent& evt);
+	void CheckSing(wxCommandEvent& evt);
 
 	wxPanel* panel;
 	wxButton* BSingUp;
 	wxButton* BLogin;
 	wxButton* Singin;
 	wxButton* rexit;
+	wxButton* add;
 	wxTextCtrl* Mailtext;
 	wxTextCtrl* PasswordText;
+	wxTextCtrl* username;
 	wxStaticText* mailtext2;
 	wxStaticText* passwordtext2;
 	wxStaticText* error;
+	wxStaticText* tc;
+	wxStaticText* fc;
+	wxStaticText* serror;
+	wxStaticText* perror;
+	wxStaticText* comp;
+	wxStaticText* user;
 };
 
